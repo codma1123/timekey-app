99a7796173bb0e442f358be7d5d387b9e3938e31
jh.lee <jh.lee@b2groups.com>
2024-02-13 23:23:43 +0900

    remove old component

--- app/components/NoSSRWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-import { Fragment, ReactNode } from "react";
-
-const NonSSRWrapper = ({ children }: { children: ReactNode }) => <Fragment>{children}</Fragment>;
-
-export default dynamic(() => Promise.resolve(NonSSRWrapper), {
-  ssr: false,
-});
