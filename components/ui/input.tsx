615871c6283a0c647bd9ad5a5fc4bf11b24d96d8
jh.lee <jh.lee@b2groups.com>
2024-02-17 14:41:50 +0900

    change input style

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { motion, MotionProps } from "framer-motion";
 import React, { ChangeEvent, HTMLAttributes, HTMLInputTypeAttribute, forwardRef, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { ControllerRenderProps, FieldError, Path, UseFormRegister } from "react-hook-form";
@@ -21,20 +22,30 @@ interface InputProps<T = any> extends MotionProps, ControllerRenderProps<any> {
 const Input = forwardRef<HTMLInputElement, InputProps>(({ className, closeable, type, inputMode, name, error, placeholder, autofocus, effect, register, ref: setRef, ...rest }, ref) => {
   const hookFormAttrs = useMemo(() => (name && register ? register(name as string) : {}), [register]);
 
+  const [isBlur, setIsBlur] = useState(false);
+
   return (
     <motion.input
+      className={cn(
+        "w-full bg-none h-14 px-0 py-0 box-border focus:border-b-2 focus:bg-none border-text rounded-none text-black transition-all text-2xl font-bold z-0",
+        isBlur && "bg-zinc-100/90 px-2 rounded-2xl opacity-40"
+      )}
       name={name as string}
       type={type}
       ref={ref}
       placeholder={placeholder}
       inputMode={inputMode}
-      className="w-full bg-none h-14 px-0 py-0 box-border focus:border-b-2 focus:bg-none border-text rounded-none text-black transition-all duration-400 text-2xl font-bold"
       {...rest}
       {...hookFormAttrs}
       value={rest.value || ""}
       onChange={(e) => {
         rest.onChange(e);
         effect && effect(e);
+      }}
+      onFocus={() => setIsBlur(false)}
+      onBlur={() => {
+        rest.onBlur();
+        setIsBlur(true);
       }}
     />
   );
