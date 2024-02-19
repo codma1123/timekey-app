a26cb1ea1e0f445002960807f8228174c089110d
jh.lee <jh.lee@b2groups.com>
2024-02-20 00:56:34 +0900

    edit page transition variants

--- components/motions/page-transition.tsx
+++ components/motions/page-transition.tsx
@@ -1,32 +1,39 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, MotionProps } from "framer-motion";
 import { ReactNode } from "react";
 
 import { usePathname } from "next/navigation";
 
-const PageTransition = ({ children }: { children: ReactNode }) => {
+interface PageTransitionProps extends MotionProps {
+  children: ReactNode;
+}
+
+const PageTransition = ({ children, variants, transition }: PageTransitionProps) => {
   const pathname = usePathname();
 
   return (
     <AnimatePresence mode={"wait"}>
       <motion.div
+        className="w-full"
         key={pathname}
         initial="initialState"
         animate="animateState"
         exit="exitState"
-        transition={{}}
-        variants={{
-          initialState: {
-            opacity: 0,
-            x: 20,
-          },
-          animateState: {
-            opacity: 1,
-            x: 0,
-            y: 0,
-          },
-        }}
+        transition={transition}
+        variants={
+          variants || {
+            initialState: {
+              opacity: 0,
+              x: 20,
+            },
+            animateState: {
+              opacity: 1,
+              x: 0,
+              y: 0,
+            },
+          }
+        }
       >
         {children}
       </motion.div>
