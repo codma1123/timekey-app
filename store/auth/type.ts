80893bd2ea802ce4a41ea4197ec80106507a38c9
jh.lee <jh.lee@b2groups.com>
2024-02-20 14:43:35 +0900

    define set Id in store

--- store/auth/type.ts
+++ store/auth/type.ts
@@ -1,27 +1,27 @@
 export interface SignUpForm {
-  name?: string
-  phoneNumber?: string
-  email?: string
-  password?: string
-  confirmPassword?: string
+  name?: string;
+  phoneNumber?: string;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
 }
 
 export interface SignInform {
-  email?: string
-  password?: string
+  email?: string;
+  password?: string;
 }
 
 export type AuthState = {
   isAuthenticate: boolean;
   user: any;
+  id: string | number;
 };
 
-export type User = {
-  
-};
+export type User = {};
 
 export type AuthActions = {
   signUp: (form: SignUpForm) => Promise<void>;
   signIn: (form: SignInform) => Promise<void>;
   signOut: () => void;
+  setId: (id: number | string) => void;
 };
