5da84348992f489f42976c1167b3db842537367f
jh.lee <jh.lee@b2groups.com>
2024-02-20 14:50:07 +0900

    title change to framer-motion

--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -1,20 +1,24 @@
 import { cn } from "@/lib/utils";
 import { IconProps } from "@radix-ui/react-icons/dist/types";
+import { motion, MotionProps } from "framer-motion";
 import { LucideIcon } from "lucide-react";
 import React from "react";
 
-interface TitleProps {
+interface TitleProps extends MotionProps {
   Icon?: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>> | LucideIcon;
   title: string;
   className?: string;
 }
 
-const Title = ({ Icon, title, className }: TitleProps) => {
+const Title = ({ Icon, title, className, ...rest }: TitleProps) => {
   return (
-    <div className={cn("py-4 text-5xl flex items-center gap-[10px] w-full font-bold", className)}>
+    <motion.div
+      className={cn("py-4 text-5xl flex items-center gap-[10px] w-full font-bold", className)}
+      {...rest}
+    >
       {title}
       {Icon && <Icon className="h-8 w-8" />}
-    </div>
+    </motion.div>
   );
 };
 
