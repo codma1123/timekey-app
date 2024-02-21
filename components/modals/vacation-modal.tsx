dd85b0783f5c95f01daf5c9ecf9d9793a52ce29c
jh.lee <jh.lee@b2groups.com>
2024-02-22 08:49:26 +0900

    add mock loading in vacation modal

--- components/modals/vacation-modal.tsx
+++ components/modals/vacation-modal.tsx
@@ -1,12 +1,17 @@
+import SlideDown from "@/components/motions/slide-down";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { delay } from "@/lib/utils";
+import { useGlobalLoading } from "@/store/global-loading";
 import { useModalStore } from "@/store/use-modal-store";
-import React from "react";
+import { useRouter } from "next/navigation";
 
 const VacationModal = () => {
-  const { isOpen, modalType, onClose } = useModalStore();
+  const { isOpen, modalType, modalData, onClose } = useModalStore();
+  const { setLoading } = useGlobalLoading();
 
   const isModalOpen = isOpen && modalType === "vacation";
+  const router = useRouter();
 
   return (
     <Dialog
@@ -16,16 +21,27 @@ const VacationModal = () => {
       <DialogContent className="sm:max-w-md bg-white w-[90%] rounded-[40px]">
         <DialogHeader>
           <DialogTitle className="text-xl">연차 신청</DialogTitle>
-          <DialogDescription className="text-lg">정말로 연차를 신청하시겠습니까?</DialogDescription>
+
+          <DialogDescription className="text-lg">
+            <SlideDown delay={0.4}>정말로 연차를 신청하시겠습니까?.</SlideDown>
+            <SlideDown delay={0.7}>
+              <p className="font-bold">2024.01.06 ~ 2024.01.07</p>
+            </SlideDown>
+          </DialogDescription>
         </DialogHeader>
 
         <DialogFooter>
           <DialogClose className="w-full flex justify-center gap-4">
             <Button
-              className="bg-zinc-700 text-lg w-28 rounded-full"
-              variant="error"
+              className="bg-emerald-400 text-lg w-28 rounded-full text-white active:bg-blue-500"
               type="button"
               size="lg"
+              onClick={async () => {
+                setLoading(true);
+                await delay(500);
+                setLoading(false);
+                router.back();
+              }}
             >
               연차 신청
             </Button>
