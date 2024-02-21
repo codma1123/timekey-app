6c63c8aecbaf51e5cfb67f09acd7e78d0dfdfdf2
jh.lee <jh.lee@b2groups.com>
2024-02-21 09:01:40 +0900

    add modal action and modalData attribute

--- store/use-modal-store.ts
+++ store/use-modal-store.ts
@@ -1,22 +1,30 @@
+import { AnyAction } from "@/store/types";
+import { DateRange } from "react-day-picker";
 import { create } from "zustand";
 
 export type ModalType = "ealryWorkoff" | "workoff" | "vacation";
 
-interface ModalData {}
+type VacationRequestModalAction = AnyAction<"vacation", DateRange>;
+
+type ModalActions = VacationRequestModalAction;
 
 interface ModalStoreState {
   isOpen: boolean;
   modalType: ModalType;
+  modalData: ModalActions["payload"] | null;
 }
 
 interface ModalStoreAction {
   onClose: () => void;
   onOpen: (modalType: ModalType) => void;
+  openWithAction: (action: ModalActions) => void;
 }
 
 export const useModalStore = create<ModalStoreState & ModalStoreAction>((set) => ({
   isOpen: false,
   modalType: null,
+  modalData: null,
   onClose: () => set({ isOpen: false }),
-  onOpen: (modalType) => set({ isOpen: true, modalType }),
+  onOpen: (modalType) => set({ isOpen: true, modalType, modalData: null }),
+  openWithAction: ({ payload, type }) => set({ isOpen: true, modalType: type, modalData: payload }),
 }));
