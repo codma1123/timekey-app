44a93011901f3d769f0ee11e39396271ca203340
jh.lee <jh.lee@b2groups.com>
2024-02-23 08:51:54 +0900

    set mock getVacations

---
+++ api/get-vacations.ts
@@ -0,0 +1,43 @@
+import { Vacation } from "@/api/types";
+import { delay } from "@/lib/utils";
+
+export const getVacations = async (): Promise<Vacation[]> => {
+  await delay(200);
+  return [
+    {
+      id: "1",
+      date: new Date(),
+      allowed: false,
+      status: "done",
+      reason: "내맘",
+    },
+    {
+      id: "2",
+      date: new Date(),
+      allowed: false,
+      status: "waiting",
+      reason: "내맘",
+    },
+    {
+      id: "3",
+      date: new Date(),
+      allowed: false,
+      status: "done",
+      reason: "내맘",
+    },
+    {
+      id: "4",
+      date: new Date(),
+      allowed: false,
+      status: "done",
+      reason: "내맘",
+    },
+    {
+      id: "5",
+      date: new Date(),
+      allowed: false,
+      status: "done",
+      reason: "내맘",
+    },
+  ];
+};
--- api/types.ts
+++ api/types.ts
@@ -1,4 +1,11 @@
+export type VacationStatus = "waiting" | "done";
+
 export type Vacation = {
   id: any;
   date: Date;
+  allowed: boolean;
+  status: VacationStatus;
+  reason: string;
 };
+
+export type VacationResponse = {};
