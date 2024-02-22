0b72f3118098ab43c337b98ce5cfc586120b4508
jh.lee <jh.lee@b2groups.com>
2024-02-23 08:53:55 +0900

    add vacation detail bottom over content

--- components/bottom-overs/vacation-detail-bottom-over.tsx
+++ components/bottom-overs/vacation-detail-bottom-over.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import ListItem from "@/components/ui/list-item";
+import { Separator } from "@/components/ui/separator";
 import { useBottomOverStore } from "@/store/bottom-over";
-import { XCircle } from "lucide-react";
+import { useModalStore } from "@/store/use-modal-store";
+import { CalendarIcon } from "@radix-ui/react-icons";
 
 const VacationDetailBottomOver = () => {
   const { isOpen, bottomOverType, bottomOverData: vacation, closeBottomOver } = useBottomOverStore();
+  const { openWithAction } = useModalStore();
 
   const isBottomOverOpen = isOpen && bottomOverType === "vacationDetail";
+
+  if (!vacation) return null;
 
   return (
     <Drawer
@@ -15,11 +22,50 @@ const VacationDetailBottomOver = () => {
       onOpenChange={(e) => !e && closeBottomOver()}
     >
       <DrawerTrigger className="hidden"></DrawerTrigger>
-      <DrawerContent className="bg-zinc-700 ring-0 outline-none border-none h-96 text-white">
-        <DrawerHeader className="text-2xl text-left">{vacation?.date.toLocaleDateString()}</DrawerHeader>
-
+      <DrawerContent className="bg-zinc-700 ring-0 outline-none border-none h-[420px] text-white">
         <div className="absolute top-2 left-[50%] h-[6px] w-36 rounded-full bg-zinc-600/50 -translate-x-1/2"></div>
-        <DrawerDescription className="px-4"></DrawerDescription>
+
+        <DrawerHeader className="text-2xl text-left flex items-center">
+          <CalendarIcon className="h-5 w-5" />
+          <span className="font-bold">연차 내역</span>
+        </DrawerHeader>
+
+        <DrawerDescription className="px-4">
+          <div className="h-[1px] bg-zinc-600/50 my-1"></div>
+
+          <ListItem
+            title="신청 날짜"
+            content={vacation.date.toLocaleDateString()}
+          />
+          <ListItem
+            title="승인 여부"
+            content={<span className={vacation.allowed ? "text-emerald-500" : "text-rose-500"}>{vacation.allowed ? "승인" : "승인 대기중"}</span>}
+          />
+          <ListItem
+            title="연차 상태"
+            content={<span className="text-yellow-500">{vacation.status}</span>}
+          />
+          <ListItem
+            title="연차 사유"
+            content={vacation.reason}
+          />
+        </DrawerDescription>
+
+        <DrawerFooter className="text-center">
+          <button
+            className="rounded-3xl bg-rose-500 text-white py-4 mb-4 text-lg"
+            onClick={() =>
+              openWithAction({
+                type: "vacation-cancel",
+                payload: {
+                  id: "",
+                },
+              })
+            }
+          >
+            연차 요청 취소
+          </button>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
   );
