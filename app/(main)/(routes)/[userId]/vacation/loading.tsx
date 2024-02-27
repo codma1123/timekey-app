bdca9d4fa4805c9c11b8085b47ea889e6fc2b810
jh.lee <jh.lee@b2groups.com>
2024-02-28 08:30:22 +0900

    clean code

--- app/(main)/(routes)/[userId]/vacation/loading.tsx
+++ app/(main)/(routes)/[userId]/vacation/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <main className="flex flex-col items-center h-screen min-w-screen gap-4 pt-16 px-6">
-      <div className={"py-4 text-5xl flex items-center gap-[10px] w-full font-bold text-zinc-200"}>연차</div>
+      <div className="py-4 text-5xl w-full font-bold text-zinc-200">연차</div>
 
       <Skeleton className="h-16 w-full mt-[13px] rounded-2xl bg-zinc-600" />
       <Skeleton className="h-16 w-full rounded-2xl bg-zinc-600" />
