d22b95e4b235b0a1d7b2d3ddb58966a290a4dd0a
jh.lee <jh.lee@b2groups.com>
2024-03-07 17:29:34 +0900

    add pull scroll

--- app/(main)/(routes)/[userId]/report/page.tsx
+++ app/(main)/(routes)/[userId]/report/page.tsx
@@ -6,78 +6,75 @@ import { Alert, AlertDescription } from "@/components/ui/alert";
 import TopLabel from "@/components/ui/top-label";
 import { cn } from "@/lib/utils";
 import StrechableAlert from "@/components/ui/stretchable-alert";
-import { getReportSummary } from "@/api/get-report";
 import ReportList from "@/app/(main)/(routes)/[userId]/(component)/report-list";
-import { motion } from "framer-motion";
-import { useState } from "react";
+import ReactPullToRefresh from "react-pull-to-refresh";
+import { getReportSummary } from "@/api/get-report";
 import { useRouter } from "next/navigation";
 
-const ReportPage = () => {
+const ReportPage = async () => {
+  const router = useRouter();
   const rate = 98;
 
-  // const { totalWorkDay, wholesomeRate } = await getReportSummary();
+  const { totalWorkDay, wholesomeRate } = await getReportSummary();
 
-  const [isRefreshing, setIsRefreshing] = useState(false);
-  const router = useRouter();
+  const onRefresh = (() => {
+    router.refresh();
+  }) as () => Promise<void>;
 
   return (
-    <motion.main
-      className="flex flex-col items-center min-h-screen min-w-screen gap-4 pt-16 px-6 pb-24 text-white"
-      drag="y"
-      dragConstraints={{ top: -20, bottom: 0 }}
-      onDrag={(_, { offset }) => {
-        if (offset.y > 50 && !isRefreshing) {
-        }
-      }}
+    <ReactPullToRefresh
+      loading={
+        <div className="loading mt-16">
+          <span className="z-[100] loading-ptr-1 bg-white text-rose-400"></span>
+          <span className="z-[100] loading-ptr-2 bg-white text-rose-400"></span>
+          <span className="z-[100] loading-ptr-3 bg-white text-rose-400"></span>
+        </div>
+      }
+      onRefresh={onRefresh}
     >
-      <TopLabel
-        scrolledClassName="bg-white top-0"
-        scrolledBackgroundColor="rgb(33 33 33)"
-        titleClassName="text-white"
-        label="근무 기록"
-      />
+      <div className="flex flex-col items-center min-h-screen min-w-screen gap-4 pt-16 px-6 pb-24 text-white">
+        <TopLabel
+          scrolledClassName="bg-white top-0"
+          scrolledBackgroundColor="rgb(33 33 33)"
+          titleClassName="text-white"
+          label="근무 기록"
+        />
 
-      <div className="w-full text-white ">{isRefreshing ? "땡기엇" : "음.."}</div>
+        <SlideDown>
+          <div className="text-xl w-full text-white font-bold mt-2 ">근무 요약</div>
+        </SlideDown>
 
-      <SlideDown>
-        <div className="text-xl w-full text-white font-bold mt-2 ">근무 요약</div>
-      </SlideDown>
-
-      <SlideDown>
-        <StrechableAlert
-          alertClassName="bg-content"
-          extend={<div className="text-center text-2xl mt-8 font-bold">222일 14시간 20일</div>}
-        >
-          <ClockIcon className="h-4 w-4" />
-          <span className="ml-2"> 총 근무일수 </span>
-          <span className="ml-auto text-2xl"> {199} 일</span>
-        </StrechableAlert>
-
-        <Alert className="border-0 bg-content rounded-2xl h-16 mt-4">
-          <AlertDescription className="pr-2 flex items-center h-full">
+        <SlideDown>
+          <StrechableAlert
+            alertClassName="bg-content"
+            extend={<div className="text-center text-2xl mt-8 font-bold">222일 14시간 20일</div>}
+          >
             <ClockIcon className="h-4 w-4" />
-            <span className="ml-2"> 근무 건전도 </span>
-            <span className={cn("ml-auto text-2xl font-bold", 98 > 95 ? "text-emerald-400" : "text-yellow-400")}> {rate} % </span>
-          </AlertDescription>
-        </Alert>
+            <span className="ml-2"> 총 근무일수 </span>
+            <span className="ml-auto text-2xl"> {totalWorkDay} 일</span>
+          </StrechableAlert>
 
-        <Alert className="border-0 bg-content rounded-2xl h-16 mt-4">
-          <AlertDescription className="pr-2 flex items-center h-full">
-            <ClockIcon className="h-4 w-4" />
-            <span className="mx-auto">이번주 3시간이나 야근했어요.</span>
-          </AlertDescription>
-        </Alert>
-      </SlideDown>
+          <Alert className="border-0 bg-content rounded-2xl h-16 mt-4">
+            <AlertDescription className="pr-2 flex items-center h-full">
+              <ClockIcon className="h-4 w-4" />
+              <span className="ml-2"> 근무 건전도 </span>
+              <span className={cn("ml-auto text-2xl font-bold", wholesomeRate > 95 ? "text-emerald-400" : "text-yellow-400")}> {rate} % </span>
+            </AlertDescription>
+          </Alert>
 
-      <div className="text-xl w-full text-white font-bold mt-2">근무 내역</div>
+          <Alert className="border-0 bg-content rounded-2xl h-16 mt-4">
+            <AlertDescription className="pr-2 flex items-center h-full">
+              <ClockIcon className="h-4 w-4" />
+              <span className="mx-auto">이번주 3시간이나 야근했어요.</span>
+            </AlertDescription>
+          </Alert>
+        </SlideDown>
 
-      <ReportList />
+        <div className="text-xl w-full text-white font-bold mt-2">근무 내역</div>
 
-      {/* <FullSheet
-        beforeSlideContent={<div>전</div>}
-        afterSlideContent={<div>후</div>}
-      /> */}
-    </motion.main>
+        <ReportList />
+      </div>
+    </ReactPullToRefresh>
   );
 };
 
