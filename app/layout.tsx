ddc2fa81eb1818c937521d7f514cf6cbe61eae0b
jh.lee <jh.lee@b2groups.com>
2024-03-13 23:36:31 +0900

    change font class tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ import { ModalProvider } from "@/components/provider/modal-provider";
 import AppProvider from "@/components/provider/app";
 import { BottomOverProvider } from "@/components/provider/bottom-over-provider";
 
+import { DOSIS } from "@/lib/fonts/dosis";
+import { IBM_KR } from "@/lib/fonts/ibm-kr";
+import { cn } from "@/lib/utils";
+
 export const metadata: Metadata = {
   title: "TIMEKEY",
   description: "TIMEKEY Generate By Codma & Somini",
@@ -25,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang="en"
       suppressHydrationWarning
+      className={cn(DOSIS.className, IBM_KR.className)}
     >
       <ModalProvider />
       <BottomOverProvider />
