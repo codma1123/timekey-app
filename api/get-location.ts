d149397ddcc926beebfd7d1ce4b6e12357b20200
jh.lee <jh.lee@b2groups.com>
2024-03-13 23:34:46 +0900

    add getlocation mock

---
+++ api/get-location.ts
@@ -0,0 +1,12 @@
+import { Location } from "@/types/location";
+
+export const getLocation = (locationId: number): Promise<Location> =>
+  new Promise((resolve) =>
+    resolve({
+      position: {
+        lat: 38,
+        lng: 127,
+      },
+      id: 2,
+    })
+  );
