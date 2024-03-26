d86829443e99621419209c883c39c823a30787f7
jh.lee <jh.lee@b2groups.com>
2024-03-27 08:55:05 +0900

    define uuid type

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
+
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
+}
+
+declare global {
+  type UUID = ReturnType<typeof uuidv4>;
 }
