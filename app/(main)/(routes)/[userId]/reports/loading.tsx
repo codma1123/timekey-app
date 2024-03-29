5fe7b7fe47ab4b7f8f0e42a5214949bdfeca0cb4
jh.lee <jh.lee@b2groups.com>
2024-03-29 10:16:45 +0900

    add skeleton

--- app/(main)/(routes)/[userId]/reports/loading.tsx
+++ app/(main)/(routes)/[userId]/reports/loading.tsx
@@ -2,12 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const ReportsLoading = () => {
   return (
-    <main className="flex flex-col items-center h-screen min-w-screen gap-4 pt-16 px-6 bg-overtime">
+    <main className="flex flex-col h-screen min-w-screen gap-4 pt-16 px-6 bg-overtime">
       <div className="py-4 text-5xl w-full font-bold text-white">근무 기록</div>
-
       <Skeleton className="h-16 w-full mt-[13px] rounded-2xl bg-content" />
       <Skeleton className="h-16 w-full rounded-2xl bg-content" />
       <Skeleton className="h-16 w-full rounded-2xl bg-content" />
+
+      <Skeleton className="h-7 w-16 mt-2 rounded-2xl bg-content self-start" />
     </main>
   );
 };
