98522696ebe8d2d3f8f0662e4d0d2285a1cc2a97
jh.lee <jh.lee@b2groups.com>
2024-03-29 10:14:33 +0900

    remove map-area top-navs

--- app/(main)/(routes)/[userId]/home/(routes)/map/(components)/map-area.tsx
+++ app/(main)/(routes)/[userId]/home/(routes)/map/(components)/map-area.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import BackButton from "@/components/ui/back-button";
+import ValidLocationAlert from "@/components/valid-location-alert";
 import { useBottomOverStore } from "@/store/bottom-over";
 
 import { Position } from "@/types/position";
@@ -8,7 +9,7 @@ import { Geolocation } from "@capacitor/geolocation";
 import { Haptics, ImpactStyle } from "@capacitor/haptics";
 import { Location } from "@prisma/client";
 import { Circle, GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-import { CheckCircle2, LocateFixed } from "lucide-react";
+import { LocateFixed } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 import { useShallow } from "zustand/react/shallow";
 
@@ -33,7 +34,6 @@ const MapArea = (props: GoogleMapAreaProps) => {
   const [isCurrentPositionLoaded, setisCurrentPositionLoaded] = useState(false);
   const [center, setCenter] = useState<google.maps.LatLngLiteral | null>(null);
   const [currentUserPosition, setCurrentUserPosition] = useState<google.maps.LatLngLiteral | null>(null);
-  const [circleData, setCircleData] = useState<Location>(null);
 
   const [openBottomOver, openBottomOverWithPayload, closeBottomOver] = useBottomOverStore(useShallow((state) => [state.openBottomOver, state.openBottomOverWithPayload, state.closeBottomOver]));
 
@@ -47,14 +47,10 @@ const MapArea = (props: GoogleMapAreaProps) => {
     map.panTo(currentUserPosition);
   }, [map, center]);
 
-  const onUnmount = useCallback((map: google.maps.Map) => {
-    setMap(null);
-  }, []);
-
   const onCenterMarkerClick = useCallback(
     (event: google.maps.MapMouseEvent) => {
       map.panTo(event.latLng);
-      map.setZoom(14);
+      map.setZoom(16);
       openBottomOver("location");
     },
     [map]
@@ -66,9 +62,8 @@ const MapArea = (props: GoogleMapAreaProps) => {
         lat: props.location.latitude,
         lng: props.location.longitude,
       });
-      map.setZoom(13);
+      map.setZoom(16);
 
-      setCircleData(location);
       openBottomOver("location");
     },
     [map]
@@ -77,7 +72,6 @@ const MapArea = (props: GoogleMapAreaProps) => {
   const onMapClick = () => {
     if (!map) return;
     closeBottomOver();
-    setCircleData(null);
   };
 
   useEffect(() => {
@@ -103,7 +97,6 @@ const MapArea = (props: GoogleMapAreaProps) => {
 
     const unSubscribe = useBottomOverStore.subscribe((state, prevState) => {
       if (state.isOpen === true && prevState.isOpen === false) {
-        setCircleData(null);
       }
     });
 
@@ -113,8 +106,6 @@ const MapArea = (props: GoogleMapAreaProps) => {
       setMap(null);
     };
   }, []);
-
-  const isValidLocation = false;
 
   return center && isLoaded && isCurrentPositionLoaded ? (
     <GoogleMap
@@ -126,24 +117,12 @@ const MapArea = (props: GoogleMapAreaProps) => {
         disableDefaultUI: true,
       }}
       center={center}
-      zoom={14}
+      zoom={16}
       onLoad={setMap}
-      onUnmount={onUnmount}
+      onUnmount={() => setMap(null)}
       onClick={onMapClick}
     >
-      <div className="absolute top-[7.5%] left-6 bg-primary-dark rounded-xl text-sm z-50 p-2 px-3 text-white">
-        {isValidLocation ? (
-          <span className="flex gap-x-2 items-center">
-            <CheckCircle2 className="w-4 h-4 text-main" />
-            현재 퇴근 지역에 위치하고 있습니다.
-          </span>
-        ) : (
-          <span className="flex gap-x-2 items-center ">
-            <CheckCircle2 className="w-4 h-4" />
-            현재 퇴근 지역에 위치하고 있지 않습니다
-          </span>
-        )}
-      </div>
+      {map && <ValidLocationAlert />}
 
       <BackButton />
 
@@ -171,36 +150,33 @@ const MapArea = (props: GoogleMapAreaProps) => {
 
       {props.locations &&
         props.locations.map((location) => (
-          <Marker
-            position={{
-              lat: location.latitude,
-              lng: location.longitude,
-            }}
-            onClick={() => onLocationMarkerClick(location)}
-            icon={{
-              url: "/assets/location-pin.svg",
-              scaledSize: {
-                height: 60,
-                width: 30,
-                equals: () => true,
-              },
-            }}
-          />
-        ))}
+          <>
+            <Circle
+              key={`${location.id}-marker`}
+              center={{ lat: location.latitude, lng: location.longitude }}
+              radius={location.radius}
+              options={{
+                strokeOpacity: 0,
+                fillOpacity: 0.1,
+                fillColor: "rgba(46,61, 233, .8)",
+              }}
+            />
 
-      {circleData && (
-        <Circle
-          onClick={() => setCircleData(null)}
-          key={circleData.id}
-          center={{ lat: circleData.latitude, lng: circleData.longitude }}
-          radius={circleData.radius}
-          options={{
-            strokeOpacity: 0,
-            fillOpacity: 0.2,
-            fillColor: "rgba(46,61, 233, .8)",
-          }}
-        />
-      )}
+            <Marker
+              key={location.id}
+              position={{ lat: location.latitude, lng: location.longitude }}
+              onClick={() => onLocationMarkerClick(location)}
+              icon={{
+                url: "/assets/location-pin.svg",
+                scaledSize: {
+                  height: 60,
+                  width: 30,
+                  equals: () => true,
+                },
+              }}
+            />
+          </>
+        ))}
     </GoogleMap>
   ) : (
     <></>
