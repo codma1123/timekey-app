ec7675f9a4dd112dd4d61f99c34c601884d6798e
jh.lee <jh.lee@b2groups.com>
2024-04-08 08:41:41 +0900

    add request done page style

--- app/(main)/(routes)/[userId]/reports/(routes)/done/page.tsx
+++ app/(main)/(routes)/[userId]/reports/(routes)/done/page.tsx
@@ -1,20 +1,28 @@
 "use client";
 
+import { MonitorCheckIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const DonePage = ({ params }: { params: { userId: UUID; id: UUID } }) => {
-  const { userId, id: reportId } = params;
+  const { userId } = params;
 
   const router = useRouter();
 
   return (
     <div className="flex flex-col items-center min-h-screen min-w-screen gap-4 pt-16 px-6 pb-24 text-white bg-overtime">
+      <div className="w-1/2 rounded-2xl aspect-square flex items-center justify-center mt-6">
+        <MonitorCheckIcon className=" w-1/2 h-1/2 aspect-square" />
+      </div>
+
+      <span className="text-2xl font-extrabold">요청이 완료되었습니다.</span>
+
       <button
+        className="w-full bg-emerald-400 rounded-2xl text-xl py-4 mt-6"
         onClick={() => {
           router.replace(`/${userId}/reports`);
         }}
       >
-        홈으로
+        홈 화면으로
       </button>
     </div>
   );
